014c60c3f5d5d07ab3aa17cffc5ad84d0dac4339
Feng Lengshun <bayazid.bustami.halim@gmail.com>
2026-03-05 23:19:37 +0700

    check for SECRET_FILE in aria2c script

--- dot_local/bin/executable_aria2-rpc-autostart.sh
+++ dot_local/bin/executable_aria2-rpc-autostart.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
+
+SECRET_FILE="/home/fenglengshun/.config/aria2-secret"
+
+if [ ! -f "$SECRET_FILE" ]; then
+  tr -dc 'A-Za-z0-9' < /dev/urandom | head -c 32 > "$SECRET_FILE"
+  chmod 600 "$SECRET_FILE"
+fi
+
 /usr/bin/aria2c \
   --enable-rpc \
-  --rpc-secret "$(cat /home/fenglengshun/.config/aria2-secret)" \
+  --rpc-secret "$(cat "$SECRET_FILE")" \
   --save-session=/home/fenglengshun/.local/share/aria2c-session \
   --save-session-interval=10 \
   --force-save=true \
